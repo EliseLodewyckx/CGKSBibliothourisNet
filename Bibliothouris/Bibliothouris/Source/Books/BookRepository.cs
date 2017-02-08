59217a6a5640cb7b623eefedcd9b719bce454b44
Kevin Smet <kevin.smet@cegeka.com>
2017-02-08 14:26:42 +0100

    added changes

--- Bibliothouris/Bibliothouris/Source/Books/BookRepository.cs
+++ Bibliothouris/Bibliothouris/Source/Books/BookRepository.cs
@@ -11,17 +11,30 @@ namespace Bibliothouris.Source.Books
     { private List<Book> books = new List<Book>();
 
 
-        public List<Book> getBookByISBN (string isbn)
-        {
-            List<Book> Isbn = new List<Book>();
-            isbn = isbn.R
-            var numbers = isbn.Where<Book>(item => Regex.Matches(item.ISBN, isbn)).ToList<Book>;
-            return numbers;
-            
+        public List<Book> getBookByISBN (string number)
+        {    
+           return books.Where<Book>(item => Regex.Matches(item.getISBN(), number).Count > 0).ToList<Book>();
+        }
 
+        public List<Book> getBookByTitle(string letters)
+        {
+            return books.Where<Book>(item => Regex.Matches(item.getTitle(), letters).Count > 0).ToList<Book>();
+        }
+        public List<Book> getBookByFirstNameAuthor(string name)
+        {
+            return books.Where<Book>(item => Regex.Matches(item.getAuthorFirstName(), name).Count > 0).ToList<Book>();
+        }
+
+        public List<Book> getBookByLastNameAuthor (string name)
+        {
+            return books.Where<Book>(item => Regex.Matches(item.getAuthorLastName(), name).Count > 0).ToList<Book>();
         }
 
 
+        public List <Book> getAllBooks()
+        {
+            return this.books;
+        }
 
 
 
