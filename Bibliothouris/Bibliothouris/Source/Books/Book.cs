db5bb4a2af0f2a59fc94a96e4174936137b34ed9
Vermeiren Sanne <sanne.vermeiren@cegeka.com>
2017-02-08 13:25:34 +0100

    book

--- Bibliothouris/Bibliothouris/Source/Books/Book.cs
+++ Bibliothouris/Bibliothouris/Source/Books/Book.cs
@@ -4,9 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bibliothouris.Source.Books
+ namespace Bibliothouris.Source.Books
 {
-    class Book
+    public class Book
     {
+        string title;
+        string ISBN;
+        string AuthorFirstName;
+        string AuthorLastName;
+
+       
+
+            public Book (string title, string ISBN, string AuthorFirstName, string AuthorLastName) {
+            this.title = title;
+            this.ISBN = ISBN;
+            this.AuthorFirstName = AuthorFirstName;
+            this.AuthorLastName = AuthorLastName;
+        }
+
+        public string getTitle() {
+            return title;
+        }
+
+        public string getISBN () {
+            return ISBN;
+        }
+
+        public string getAuthorFirstName() {
+            return AuthorFirstName;
+        }
+
+        public string getAuthorLastName() {
+            return AuthorLastName;
+        }
+
+        public string getDetails () {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Title: " + getTitle() + "\r\n");
+            sb.Append("ISBN: " + getISBN() + "\r\n");
+            sb.Append("Author: " + getAuthorFirstName() + " " + getAuthorLastName());
+
+            return sb.ToString();
+        }
+
+        }
     }
-}
+
