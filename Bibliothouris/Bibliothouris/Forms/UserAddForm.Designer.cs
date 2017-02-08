3083d6222ce41c84a63a4a5a0c528f43706935e2
Elise Lodewyckx <elise.lodewyckx@cegeka.com>
2017-02-08 11:11:53 +0100

    added classes

--- Bibliothouris/Bibliothouris/Forms/UserAddForm.Designer.cs
+++ Bibliothouris/Bibliothouris/Forms/UserAddForm.Designer.cs
@@ -63,7 +63,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 66);
+            this.ClientSize = new System.Drawing.Size(347, 86);
             this.Controls.Add(this.add);
             this.Controls.Add(this.userName);
             this.Controls.Add(this.label1);
