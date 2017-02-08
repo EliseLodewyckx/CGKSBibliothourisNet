677ae2b0990b175dd5c1c89712e3b32e4d5052af
Kevin Smet <kevin.smet@cegeka.com>
2017-02-08 13:52:46 +0100

    added changes

--- Bibliothouris/Bibliothouris/Source/Books/BookRepository.cs
+++ Bibliothouris/Bibliothouris/Source/Books/BookRepository.cs
@@ -2,12 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Bibliothouris.Source.Books
 {
     class BookRepository
-    {
+    { private List<Book> books = new List<Book>();
+
+
+        public List<Book> getBookByISBN (string isbn)
+        {
+            List<Book> Isbn = new List<Book>();
+            isbn = isbn.R
+            var numbers = isbn.Where<Book>(item => Regex.Matches(item.ISBN, isbn)).ToList<Book>;
+            return numbers;
+            
+
+        }
+
+
+
+
+
+
 
     }
 }
+/*var qry = list
+    .Where<string>(item => regEx.IsMatch(item))
+    .ToList<string>();
+
+ */
