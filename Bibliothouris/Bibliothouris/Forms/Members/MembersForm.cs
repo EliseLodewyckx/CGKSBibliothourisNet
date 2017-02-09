990468ae97061c14542fa36bdf258027840ce3b1
Vermeiren Sanne <sanne.vermeiren@cegeka.com>
2017-02-09 11:27:00 +0100

    formmember

--- Bibliothouris/Bibliothouris/Forms/Members/MembersForm.cs
+++ Bibliothouris/Bibliothouris/Forms/Members/MembersForm.cs
@@ -9,9 +9,42 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Bibliothouris.Forms.Members {
-    public partial class MembersForm : Form {
+
+    public interface IMemberView {
+        void SetController(MemberController memberController);
+    }
+
+
+
+
+
+    public partial class MembersForm : Form, IMemberView {
+
+        private MemberController memberController;
+
         public MembersForm() {
             InitializeComponent();
+        }
+
+        public void SetController (MemberController memberController) {
+            this.memberController = memberController;
+        }
+        private ListViewItem CreateMemberListViewItem(Member member) {
+            ListViewItem memberItem = new ListViewItem(member.INSZ);
+            memberItem.SubItems.Add(member.FirstName);
+            memberItem.SubItems.Add(member.LastName);
+            memberItem.SubItems.Add(member.City);
+            return memberItem;
+           
+        }
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e) {
+
         }
     }
 }
