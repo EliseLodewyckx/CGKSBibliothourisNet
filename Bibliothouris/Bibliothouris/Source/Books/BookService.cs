d5e690c73a9db0f9fea2e6fa2d6a86cd4b4a8d43
Kevin Smet <kevin.smet@cegeka.com>
2017-02-09 14:53:23 +0100

    Added empty bookservice

---
+++ Bibliothouris/Bibliothouris/Source/Books/BookService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliothouris.Source.Books
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+
+        void AddBook(string txtIsbnText, string txtTitleText, string txtFirstNameText, string txtLastNameText);
+        List<Book> SearchBooks(string isbn, string title, string firstName, string lastName);
+    }
+
+    class BookService: IBookService
+    {
+        public List<Book> GetAllBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBook(string txtIsbnText, string txtTitleText, string txtFirstNameText, string txtLastNameText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> SearchBooks(string isbn, string title, string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
